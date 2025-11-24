471e7f31e7a6f0525274449a622073bc00ae5d97
lkt <jingjw529@gmail.com>
2025-11-24 02:26:38 -0800

    created new component for calendar chat page

---
+++ react-test/src/app/components/CalendarPage.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import Chat from "./Chat";
+import GoogleCalendar from "./GoogleCalendar";
+
+export default function CalendarPage() {
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  const handleEventInserted = () => {
+    setRefreshKey((prev) => prev + 1);
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row min-h-screen bg-gray-100 p-4 gap-4">
+      <div className="flex-1 max-w-md">
+        <Chat onEventUpdated={handleEventInserted} />
+      </div>
+
+      <GoogleCalendar
+        refreshKey={refreshKey}
+      />
+    </div>
+  );
+}
